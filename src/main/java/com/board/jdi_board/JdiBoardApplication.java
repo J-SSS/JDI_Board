7de1400180dfe39c8874c9dd629fb2572ad9f0e0
J-SSS <dongr5102@naver.com>
2023-05-11 11:13:04 +0900

    기본 설정 및 저장소 생성

---
+++ src/main/java/com/board/jdi_board/JdiBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.jdi_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdiBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdiBoardApplication.class, args);
+    }
+
+}
